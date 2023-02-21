89689e448a04fdec705a96911ab0cf1312a42286
tiffttu <tt2714@columbia.edu>
2023-02-20 21:39:28 -0500

    add SKAT scripts and replication genelist script

---
+++ association_analyses/skat_ANNOVAR_pipeline.R
@@ -0,0 +1,35 @@
+### annotate data using ANNOVAR first, requires vcf file
+
+## converts vcf to "avinput" file, takes a long time
+# perl table_annovar.pl ../all_ssns_ctr_hwe_mac20.vcf tempdir/ -buildver hg38 -out all_ssns_anno -remove -protocol wgEncodeGencodeBasicV26 -operation g -nastring . -vcfinput -polish --verbose
+# awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17}' all_ssns_anno.avinput > all_ssns_anno_cols.avinput
+# perl table_annovar.pl all_ssns_anno_cols.avinput dir/ -buildver hg38 -outfile all_ssns_anno_step2_0215 -remove -protocol wgEncodeGencodeBasicV26 -operation g -nastring . -polish --verbose -otherinfo
+
+### extract gene group info we need for SMMAT/SKAT analysis
+file = read.table("all_ssns_anno_step2_0215.hg38_multianno.txt", sep = "\t", header = TRUE)
+file_filter = file %>% 
+  filter(Func.wgEncodeGencodeBasicV26 == "exonic" | Func.wgEncodeGencodeBasicV26 == "exonic;splicing") %>% 
+  filter(ExonicFunc.wgEncodeGencodeBasicV26 != "nonsynonymous SNV" | ExonicFunc.wgEncodeGencodeBasicV26 != "unknown") %>% 
+  select(Chr, Otherinfo5, Ref, Alt, Func.wgEncodeGencodeBasicV26, Gene.wgEncodeGencodeBasicV26)
+
+gene_group = separate_rows(file_filter, Gene.wgEncodeGencodeBasicV26 ,sep = ";") %>% select(-Func.wgEncodeGencodeBasicV26) %>% distinct(.keep_all = TRUE)
+
+length(unique(gene_group$Gene.wgEncodeGencodeBasicV26))
+
+write.table(gene_group, "gene_group.txt", row.names=FALSE, quote=FALSE, col.names=TRUE)
+
+### SMMAT requires gds file
+bed.fn <- "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr_hwe_mac20.bed"
+fam.fn <- "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr_hwe_mac20.fam"
+bim.fn <- "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr_hwe_mac20.bim"
+library(SeqArray)
+seqBED2GDS(bed.fn, fam.fn, bim.fn, "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr/mac_20/SKAT/allssns_mac20_seq.gds")
+
+### run skat_allPC_20.R file, ~ 12 hours
+
+
+### read output file from SMMAT
+all_skat <- read.table("allssns_PC20_smmat_sameweight.txt", sep = " ", stringsAsFactors=FALSE, quote = "", header = FALSE)
+names(all_skat) = c("variant", "variant_N", "min_missing", "mean_missing", "max_missing", "min_AF", "mean_AF", "max_AF", "SKAT_PVAL")
+write_df = all_skat %>% arrange(SKAT_PVAL) %>% filter(SKAT_PVAL < 3e-6)
+write.table(write_df, "variantanalysis_0220.txt", row.names=FALSE, quote=FALSE, col.names=TRUE, sep = "\t")
---
+++ association_analyses/skat_allPC_20.R
@@ -0,0 +1,38 @@
+library(GMMAT)
+library(genio)
+library(tidyverse)
+library(SNPRelate)
+
+# prep SMMAT input
+# extract group file for european data
+bim <- read_bim("/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr_hwe_mac20.bim")
+bim_clean = bim %>% select(Chr = chr, Otherinfo5 = pos, Alt = alt, Ref = ref, id)
+gene_set = read.table("/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr/mac_20/SKAT/annovar/gene_group.txt", sep = " ", header = TRUE)
+group_file = merge(bim_clean, gene_set, by = c("Chr", "Otherinfo5", "Ref", "Alt"), all.x = TRUE) %>% drop_na(Gene.wgEncodeGencodeBasicV26) %>% mutate(weight = 1) %>% select(Gene.wgEncodeGencodeBasicV26, Chr, Otherinfo5, Ref, Alt, weight)
+group_file = group_file[order(group_file$Gene.wgEncodeGencodeBasicV26),]
+print(length(unique(group_file$Gene.wgEncodeGencodeBasicV26)))
+print(is.unsorted(group_file$Gene.wgEncodeGencodeBasicV26))
+group.file <- "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr/mac_20/SKAT/group_file_allPC20.txt"
+write.table(group_file, group.file, row.names=FALSE, quote=FALSE, col.names=FALSE,sep = "\t")
+# variant_list = group_file %>% select(id)
+# write.table(variant_list, "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr/mac_20/SKAT/smmat_w_subset.txt", row.names=FALSE, quote=FALSE, col.names=FALSE)
+
+# prep model equation:
+name <- "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/case_control/split_filter/ssns_ctr.grm"
+grm = as.matrix(read_grm(name)$kinship)
+
+phenofile = read.table("/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/case_control/ssns_ctr_pheno.txt", sep = " ", stringsAsFactors=FALSE, quote = "", header = FALSE) %>% mutate(V3 = ifelse(V3 == 1, 0, 1))
+covarfile = read.table("/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/case_control/ssns_ctr_covar.txt", sep = " ", stringsAsFactors=FALSE, quote = "", header = FALSE) 
+eigenfile = read.table("/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/case_control/split_filter/ssns_ctr.eigenvec", sep = " ", stringsAsFactors=FALSE, quote = "", header = FALSE) 
+pheno = merge(phenofile %>% rename(trait = V3), covarfile %>% rename(sex = V3, race = V4), by = c("V1","V2")) 
+pheno_all = merge(pheno, eigenfile, by = c("V1","V2")) %>% rename(famid = V1, id = V2)
+model0 <- glmmkin(trait ~  sex + race + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10 + V11 + V12, data = pheno_all, kins = grm, id = "id",family = binomial(link = "logit"))
+
+
+geno.file <- "/datacommons/ochoalab/ssns_gwas/nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/GMMAT/ssns_ctr/mac_20/SKAT/allssns_mac20_seq.gds"
+
+print("start SMMAT")
+# SMMAT
+out <- SMMAT(model0, group.file = group.file,  geno.file = geno.file, MAF.range = c(1e-7, 0.5), miss.cutoff = 1, method = "davies", tests = "S", use.minor.allele = TRUE)
+write.table(out, "SMMAT_out/allssns_PC20_smmat_sameweight.txt", row.names=FALSE, quote=FALSE, col.names=FALSE)
+print("SMMAT done")
