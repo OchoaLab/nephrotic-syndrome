fa5bfe02f089c6b787879ffcbe1786ca6c3ba342
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2023-06-08 23:23:42 -0400

    code: adapted batch wald/OR code for SSNS vs control

--- prs/gmmat_OR_ssns_ctrl_batch.R
+++ prs/gmmat_OR_ssns_ctrl_batch.R
@@ -1,5 +1,5 @@
 # original downloaded from here:
-# /datacommons/ochoalab/ssns_gwas/GMMAT_0418/PRS/gmmat_OR_srns_ssns.R
+# /datacommons/ochoalab/ssns_gwas/GMMAT_0418/ssns_ctr/gmmat_all_ssns_PC_20.R
 
 library(GMMAT)
 library(genio)
@@ -11,13 +11,13 @@ p_cut <- 1e-4
 n_batches <- 100
 
 # numbers for reference, on score test summary stats table before subsetting
-## nrow( snps )            # [1] 12256040
-## sum( snps$PVAL < 1e-4 ) # [1] 2650
-## sum( snps$PVAL < 1e-5 ) # [1] 1024
-## sum( snps$PVAL < 1e-6 ) # [1] 587
-## sum( snps$PVAL < 1e-7 ) # [1] 215
-## sum( snps$PVAL < 1e-8 ) # [1] 41
-## sum( snps$PVAL < 1e-9 ) # [1] 3
+## nrow( snps )            # [1] 20774220
+## sum( snps$PVAL < 1e-4 ) # [1] 11116
+## sum( snps$PVAL < 1e-5 ) # [1] 7389
+## sum( snps$PVAL < 1e-6 ) # [1] 5842
+## sum( snps$PVAL < 1e-7 ) # [1] 4366
+## sum( snps$PVAL < 1e-8 ) # [1] 3944
+## sum( snps$PVAL < 1e-9 ) # [1] 3169
 
 # get batch number from terminal
 args <- commandArgs( trailingOnly = TRUE )
@@ -32,17 +32,17 @@ if ( batch > n_batches )
 # paths
 # overall shared base
 base <- '/datacommons/ochoalab/ssns_gwas/'
-dir_srns <- paste0( base, 'GMMAT_0418/srns_ctr/srns_ssns/' )
+dir_ssns <- paste0( base, "GMMAT_0418/ssns_ctr/" )
 # for genotypes, GRM and PCs
-name <- paste0( dir_srns, "srns_ssns_mac20" )
+name <- paste0( dir_ssns, "ssns_ctr_mac20" )
 # score test output, needed for prioritizing SNPs
-file_gwas_score <- paste0( dir_srns, 'glmm.score.bed.srns_ssns_PC_mac20_nohwe.txt' )
+file_gwas_score <- paste0( dir_ssns, "glmm.score.bed.all_ssns_ctr_mac20_PC_nohwe.txt" )
 # phenotype and covariates are elsewhere
 dir_pheno <- paste0( base, 'nephrotic.syndrome.gwas_proprocessing_202205/allele_freq/newfiles_alpha/imputation_10_new/results/case_control/' )
-file_phen <- paste0( dir_pheno, 'ssns_srns_pheno.txt' ) # need extension if not .phen
-file_covar <- paste0( dir_pheno, "ssns_srns_covar.txt" )
+file_phen <- paste0( dir_pheno, "ssns_ctr_pheno.txt" ) # need extension if not .phen
+file_covar <- paste0( dir_pheno, "ssns_ctr_covar.txt" )
 # change output path!
-file_out <- paste0( base, "GMMAT_0418/PRS/glmm.wald_srns_ssns_batch-", batch, '-', n_batches, ".txt" )
+file_out <- paste0( base, "GMMAT_0418/PRS/glmm.wald_ssns_ctrl_batch-", batch, '-', n_batches, ".txt" )
 
 # load and process phenotype data
 phen <- read_phen( file_phen )
@@ -59,9 +59,14 @@ grm <- read_grm( name )
 eigenvec <- read_eigenvec( name )
 
 # make sure data is aligned
-stopifnot( all( data$id == phen$id ) )
 stopifnot( all( data$id == grm$fam$id ) )
 stopifnot( all( data$id == eigenvec$fam$id ) )
+#stopifnot( all( data$id == phen$id ) ) # these two aren't aligned but have same length, they're just scrambled
+stopifnot( all( data$id %in% phen$id ) )
+# order phen file, only one that disagrees with rest
+indexes <- match( data$id, phen$id )
+phen <- phen[ indexes, ]
+stopifnot( all( data$id == phen$id ) ) # now they're aligned
 
 # construct data for model
 data$trait <- phen$pheno
