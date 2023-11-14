b42f0ab3bfe5dd526e38416f84c2e07be5bdc03c
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2023-11-13 20:19:57 -0500

    ldpred: updated to use latest srns-ssns summary stats (using score test transformed to estimate coefficients), subset to array SNPs for speed, saves and reloads correlation matrix

--- prs/ldpred.R
+++ prs/ldpred.R
@@ -1,6 +1,7 @@
 # start interactive shell
 # srun --mem 16G -p ochoalab --account ochoalab --pty bash -i
 # module load R/4.1.1-rhel8
+# R
 
 # based on this tutorial
 # https://privefl.github.io/bigsnpr/articles/LDpred2.html
@@ -15,9 +16,12 @@ library(dplyr)
 setwd('/datacommons/ochoalab/ssns_gwas/replication/bristol_data/GMMAT')
 # name of data to predict on
 name <- 'srns_ssns_mac20'
-# base summary stats
-file_sumstats <- "/datacommons/ochoalab/ssns_gwas/GMMAT_0418/PRS/glmm.wald_srns_ssns.txt.gz"
 file_phen <- 'srns_ssns.phen'
+# base summary stats
+file_sumstats <- '/datacommons/ochoalab/ssns_gwas/imputed/ssns_srns/mac20-glmm-score.txt'
+#file_sumstats <- "/datacommons/ochoalab/ssns_gwas/GMMAT_0418/PRS/glmm.wald_srns_ssns.txt.gz"
+# file to subset to clean array SNPs (which passed QC already)
+name_array <- '/datacommons/ochoalab/ssns_gwas/array/ssns_tgp_merge_clean'
 
 # load validation dataset
 # generate this if it doesn't exist
@@ -46,70 +50,119 @@ G   <- obj.bigSNP$genotypes
 CHR <- obj.bigSNP$map$chromosome
 POS <- obj.bigSNP$map$physical.pos
 y   <- obj.bigSNP$fam$affection
-NCORES <- nb_cores() # on DCC this is 1 with default settings, which is probably fine because ours is a small dataset
-#NCORES <- 1 # was needed locally because of automatic blas multithreading leading to two levels of multithreading, something like that
-
-# decide how many individuals are in each subset... example had 503 indivs total, our data has slightly fewer, 490
-# only used for LDpred2-grid and lassosum2
-ind.val <- sample( nrow(G), nrow(G) * 0.7 )   # example 350 (70%)
-ind.test <- setdiff( rows_along(G), ind.val ) # example 153 (30%)
+#NCORES <- nb_cores() # on DCC this is 1 with default settings, which is probably fine because ours is a small dataset
+NCORES <- 1 # was needed locally because of automatic blas multithreading leading to two levels of multithreading, something like that
+#NCORES <- 20 # for DCC run for precalculating LD matrix
+message( NCORES )
 
 # rename columns to what snp_match wants
 map <- setNames(obj.bigSNP$map[-3], c("chr", "rsid", "pos", "a1", "a0"))
+
+# load summary statistics (big file because it's from imputed data)
 sumstats <- bigreadr::fread2( file_sumstats )
-sumstats <- setNames( sumstats[ -c(3, 12) ], c('chr', 'rsid', 'pos', 'a1', 'a0', 'n_eff', 'af', 'beta', 'beta_se', 'p') )
+# apply Debo's transformation
+sumstats$beta <- sumstats$SCORE / sumstats$VAR
+sumstats$beta_se <- 1 / sqrt( sumstats$VAR )
+# OLD: exclude "cM", "converged"
+#sumstats <- setNames( sumstats[ -c(3, 12) ], c('chr', 'rsid', 'pos', 'a1', 'a0', 'n_eff', 'af', 'beta', 'beta_se', 'p') )
+# exclude "MISSRATE" (there's no missingness here), SCORE, VAR
+stopifnot( all( sumstats$MISSRATE == 0 ) )
+# NOTE: a1, a0 could be reversed!
+#sumstats <- setNames( sumstats[ -c(7, 9, 10) ], c('rsid', 'chr', 'pos', 'a1', 'a0', 'n_eff', 'af', 'p', 'beta', 'beta_se') )
+sumstats <- setNames( sumstats[ -c(7, 9, 10) ], c('rsid', 'chr', 'pos', 'a0', 'a1', 'n_eff', 'af', 'p', 'beta', 'beta_se') )
+
+# load array SNP set
+bim <- read_bim( name_array )
+# change names to match snp_match
+#names( bim ) <- c('chr', 'rsid', 'a0', 'a1')
+names( bim )[ names( bim ) == 'id' ] <- 'rsid'
+names( bim )[ names( bim ) == 'alt' ] <- 'a1'
+names( bim )[ names( bim ) == 'ref' ] <- 'a0'
+# remove unneeded column
+bim[ names( bim ) == 'posg' ] <- NULL
+# convert chr to integer
+bim$chr <- as.integer( bim$chr )
+# find matching subset
+sumstats2 <- snp_match( sumstats, bim )
+## 12,274,557 variants to be matched.
+## 76,584 ambiguous SNPs have been removed.
+## 635,789 variants have been matched; 0 were flipped and 0 were reversed.
+nrow( bim ) # 761,366 # for reference
+
+# before next round, correct some funny business with snp_match's output
+sumstats2[ names( sumstats2 ) == 'rsid' ] <- NULL # IDs from BIM, don't want
+names( sumstats2 )[ names( sumstats2 ) == 'rsid.ss' ] <- 'rsid' # restore original IDs
+sumstats2[ names( sumstats2 ) == '_NUM_ID_.ss' ] <- NULL # new column added, also don't want
+sumstats2[ names( sumstats2 ) == '_NUM_ID_' ] <- NULL # new column added, also don't want
 
 # map assuming things are largely aligned already
-df_beta <- snp_match(sumstats, map)
-## 13,971 variants to be matched.
-## 1,410 ambiguous SNPs have been removed.
-## 8,167 variants have been matched; 0 were flipped and 0 were reversed.
+df_beta <- snp_match( sumstats2, map )
+## 635,789 variants to be matched.
+## 0 ambiguous SNPs have been removed.
+## 492,982 variants have been matched; 0 were flipped and 0 were reversed.
 
 # here processing suggests QC on sumstats, but no code is provided (there's equations and a massive repo, may have to revisit)
 
 # convert basepairs to genetic position this way
 POS2 <- snp_asGeneticPos(CHR, POS, ncores = NCORES)
 
-# compute correlations (LD matrices) on the fly
-for (chr in 1:22) {
-    message(chr)
-    ## indices in 'df_beta'
-    ind.chr <- which(df_beta$chr == chr)
-    ## indices in 'G'
-    ind.chr2 <- df_beta$`_NUM_ID_`[ind.chr]
-    corr0 <- snp_cor(G, ind.col = ind.chr2, size = 3 / 1000,
-                     infos.pos = POS2[ind.chr2], ncores = NCORES)
-    if (chr == 1) {
-        ld <- Matrix::colSums(corr0^2)
-        corr <- as_SFBM(corr0, compact = TRUE)
-    } else {
-        ld <- c(ld, Matrix::colSums(corr0^2))
-        corr$add_columns(corr0, nrow(corr))
+# corr backing file, to reload if needed
+corr_file <- paste0( name, '-corr' )
+#corr_file <- paste0( name, '-corr-TEST' )
+# NOTE: code blindly adds another .sbk extension if I specify one (leading to double '.sbk.sbk"), so don't include it
+# these ones have added extension for use internally
+#corr_file_sbk <- paste0( corr_file, '.sbk' ) # NOT USED
+corr_file_rdata <- paste0( corr_file, '.RData' )
+
+if ( file.exists( corr_file_rdata ) ) {
+    # load from a different session
+    load( corr_file_rdata )
+} else {
+    # compute correlations (LD matrices) on the fly
+    for (chr in 1:22) {
+        message(chr)
+        ## indices in 'df_beta'
+        ind.chr <- which(df_beta$chr == chr)
+        ## indices in 'G'
+        ind.chr2 <- df_beta$`_NUM_ID_`[ind.chr]
+        # calculate sparse symmetric correlation matrix
+        corr0 <- snp_cor(G, ind.col = ind.chr2, size = 3 / 1000,
+                         infos.pos = POS2[ind.chr2], ncores = NCORES)
+        # calculate LD scores
+        ld0 <- Matrix::colSums(corr0^2)
+        # combine data across chromosomes
+        if (chr == 1) {
+            ld <- ld0
+            # set up backing file, first time only
+            corr <- as_SFBM(corr0, corr_file, compact = TRUE)
+        } else {
+            ld <- c(ld, ld0)
+            corr$add_columns(corr0, nrow(corr))
+        }
     }
+    # save object, keep backing file, so we can load it in another session
+    save( corr, file = corr_file_rdata )
 }
-# yey this is small, probably because our summary stats are so sparse
+
+# size of LD data, in GB
 file.size(corr$sbk) / 1024^3  # file size in GB
-#[1] 0.02784605
+#[1] 4.118546
+
+# decide how many individuals are in each subset... example had 503 indivs total, our data has slightly fewer, 490
+# only used for LDpred2-grid and lassosum2
+ind.val <- sample( nrow(G), nrow(G) * 0.7 )   # example 350 (70%)
+ind.test <- setdiff( rows_along(G), ind.val ) # example 153 (30%)
 
 ### ldpred2-inf
 
 # Estimate of h2 from LD Score regression
 ldsc <- with(df_beta, snp_ldsc(ld, length(ld), chi2 = (beta / beta_se)^2, sample_size = n_eff, blocks = NULL))
-# ok, something is obviously wrong with this absurdly large h2, we need more summary stats!
-##          int           h2 
-## 1.163067e+01 4.105297e+14 
-#h2_est <- ldsc[["h2"]]
-h2_est <- 0.5 # choose something nice in the middle, the above fails totally!
+h2_est <- ldsc[["h2"]]
 beta_inf <- snp_ldpred2_inf(corr, df_beta, h2 = h2_est)
-## Warning message: (for original h2_est)
-## In sp_solve_sym_eigen(A, b, add_to_diag, tol, maxiter) :
-##   Estimated error: 0.0119041.
 pred_inf <- big_prodVec(G, beta_inf, ind.row = ind.test, ind.col = df_beta[["_NUM_ID_"]])
 pcor(pred_inf, y[ind.test], NULL)
-# [1] NA NA NA # both ways!
-cor(pred_inf, y[ind.test])
-# [1] 0.06213717
-# [1] 0.115047 # second run!
+# [1]  0.04728706 -0.11549036  0.20759114
+#cor(pred_inf, y[ind.test])
 
 ### ldpred2-grid
 
@@ -126,48 +179,47 @@ params$score <- apply(pred_grid[ind.val, ], 2, function(x) {
 
 fig_start( 'ldpred2-grid', width = 6 )
 ggplot(params, aes(x = p, y = score, color = as.factor(h2))) +
-  theme_bigstatsr() +
-  geom_point() +
-  geom_line() +
-  scale_x_log10(breaks = 10^(-5:0), minor_breaks = params$p) +
-  facet_wrap(~ sparse, labeller = label_both) +
-  labs(y = "GLM Z-Score", color = "h2") +
+    theme_bigstatsr() +
+    geom_point() +
+    geom_line() +
+    scale_x_log10(breaks = 10^(-5:0), minor_breaks = params$p) +
+    facet_wrap(~ sparse, labeller = label_both) +
+    labs(y = "GLM Z-Score", color = "h2") +
     theme(legend.position = "top", panel.spacing = unit(1, "lines"))
 fig_end()
 
 params %>%
-  mutate(sparsity = colMeans(beta_grid == 0), id = row_number()) %>%
-  arrange(desc(score)) %>%
-  mutate_at(c("score", "sparsity"), round, digits = 3) %>%
+    mutate(sparsity = colMeans(beta_grid == 0), id = row_number()) %>%
+    arrange(desc(score)) %>%
+    mutate_at(c("score", "sparsity"), round, digits = 3) %>%
     slice(1:10)
-##          p   h2 sparse score sparsity  id
-## 1  0.00180 0.70   TRUE 1.351    0.011 157
-## 2  0.00100 0.70   TRUE 1.351    0.004 156
-## 3  0.00100 0.35   TRUE 1.351    0.042 114
-## 4  0.00100 0.50   TRUE 1.351    0.087 135
-## 5  0.00056 0.35   TRUE 1.351    0.133 113
-## 6  0.00056 0.70   TRUE 1.351    0.102 155
-## 7  0.00056 0.35  FALSE 1.351    0.000  29
-## 8  0.00056 0.50   TRUE 1.351    0.020 134
-## 9  0.00032 0.50  FALSE 1.350    0.000  49
-## 10 0.00180 0.70  FALSE 1.350    0.000  73
+##          p     h2 sparse score sparsity  id
+## 1  1.0e-05 0.3391  FALSE 5.059    0.000   1
+## 2  5.6e-05 0.7913   TRUE 4.930    0.921 109
+## 3  3.2e-05 1.5826   TRUE 4.619    0.962 150
+## 4  1.0e-04 0.3391  FALSE 4.322    0.000   5
+## 5  1.8e-04 1.5826  FALSE 4.280    0.000  69
+## 6  5.6e-05 0.3391  FALSE 4.243    0.000   4
+## 7  3.2e-05 0.3391   TRUE 4.185    0.910  87
+## 8  5.6e-05 1.1305   TRUE 4.176    0.932 130
+## 9  3.2e-05 1.5826  FALSE 4.153    0.000  66
+## 10 3.2e-05 1.1305  FALSE 4.150    0.000  45
 best_beta_grid <- params %>%
-  mutate(id = row_number()) %>%
-  # filter(sparse) %>% 
-  arrange(desc(score)) %>%
-  slice(1) %>%
-  print() %>% 
-  pull(id) %>% 
+    mutate(id = row_number()) %>%
+    # filter(sparse) %>% 
+    arrange(desc(score)) %>%
+    slice(1) %>%
+    print() %>% 
+    pull(id) %>% 
     beta_grid[, .]
-##        p  h2 sparse   score  id
-## 1 0.0018 0.7   TRUE 1.35105 157
+##       p     h2 sparse    score id
+## 1 1e-05 0.3391  FALSE 5.059323  1
 pred <- big_prodVec(G, best_beta_grid, ind.row = ind.test,
                     ind.col = df_beta[["_NUM_ID_"]])
 pcor(pred, y[ind.test], NULL)
-# [1] NA NA NA
-cor(pred, y[ind.test])
-# [1] 0.0666571
-# [1] 0.1165977 # second run!
+# [1]  0.12688295 -0.03574459  0.28296373
+#cor(pred, y[ind.test])
+
 
 ### ldpred2-auto
 
@@ -186,33 +238,43 @@ keep <- which(range > (0.95 * quantile(range, 0.95, na.rm = TRUE)))
 beta_auto <- rowMeans(sapply(multi_auto[keep], function(auto) auto$beta_est))
 pred_auto <- big_prodVec(G, beta_auto, ind.row = ind.test, ind.col = df_beta[["_NUM_ID_"]])
 pcor(pred_auto, y[ind.test], NULL)
-# [1] NA NA NA
-cor(pred_auto, y[ind.test])
-# [1] 0.1156314
+# [1]  0.07659266 -0.08637162  0.23556498
+#cor(pred_auto, y[ind.test])
+
+#save.image()
+
+############################## STOPPED HERE!!!
+
 
 ### lassosum2
 
 beta_lassosum2 <- snp_lassosum2(corr, df_beta, ncores = NCORES)
 params2 <- attr(beta_lassosum2, "grid_param")
+pred_grid2 <- big_prodMat(G, beta_lassosum2, ind.col = df_beta[["_NUM_ID_"]])
+params2$score <- apply(pred_grid2[ind.val, ], 2, function(x) {
+    if (all(is.na(x))) return(NA)
+    summary(lm(y[ind.val] ~ x))$coef["x", 3]
+    # summary(glm(y[ind.val] ~ x, family = "binomial"))$coef["x", 3]
+})
 
 # this plot is very different than example, it just looks bad
 fig_start( 'lassosum2', width = 6 )
 ggplot(params2, aes(x = lambda, y = score, color = as.factor(delta))) +
-  theme_bigstatsr() +
-  geom_point() +
-  geom_line() +
-  scale_x_log10(breaks = 10^(-5:0)) +
-  labs(y = "GLM Z-Score", color = "delta") +
-  theme(legend.position = "top") +
-  guides(colour = guide_legend(nrow = 1))
+    theme_bigstatsr() +
+    geom_point() +
+    geom_line() +
+    scale_x_log10(breaks = 10^(-5:0)) +
+    labs(y = "GLM Z-Score", color = "delta") +
+    theme(legend.position = "top") +
+    guides(colour = guide_legend(nrow = 1))
 fig_end()
 
 best_grid_lassosum2 <- params2 %>%
-  mutate(id = row_number()) %>%
-  arrange(desc(score)) %>%
-  print() %>% 
-  slice(1) %>%
-  pull(id) %>% 
+    mutate(id = row_number()) %>%
+    arrange(desc(score)) %>%
+    print() %>% 
+    slice(1) %>%
+    pull(id) %>% 
     beta_lassosum2[, .]
 best_grid_overall <- 
   `if`(max(params2$score, na.rm = TRUE) > max(params$score, na.rm = TRUE),
@@ -221,16 +283,16 @@ best_grid_overall <-
 pred <- big_prodVec(G, best_grid_lassosum2, ind.row = ind.test,
                     ind.col = df_beta[["_NUM_ID_"]])
 pcor(pred, y[ind.test], NULL)
-# [1] NA NA NA
-cor(pred, y[ind.test])
-# [1] 0.1162437 # second run only
+# [1]  0.10867889 -0.05416744  0.26589394
+#cor(pred, y[ind.test])
 
 pred <- big_prodVec(G, best_grid_overall, ind.row = ind.test,
                     ind.col = df_beta[["_NUM_ID_"]])
 pcor(pred, y[ind.test], NULL)
-# [1] NA NA NA
-cor(pred, y[ind.test])
-# [1] 0.1165977 # second run only
+# [1]  0.12688295 -0.03574459  0.28296373
+#cor(pred, y[ind.test])
+
+#save.image()
 
 
 ### ldpred2-auto, pt 2
@@ -238,34 +300,33 @@ cor(pred, y[ind.test])
 # reuses up to `keep` above
 all_h2 <- sapply(multi_auto[keep], function(auto) tail(auto$path_h2_est, 500))
 quantile(all_h2, c(0.5, 0.025, 0.975))
-# all of these are way higher for me than they are in example
-##      50%     2.5%    97.5% 
-## 24.78544 23.91517 25.62840 
+##       50%      2.5%     97.5% 
+## 0.7597156 0.1502886 1.3498973 
 all_p <- sapply(multi_auto[keep], function(auto) tail(auto$path_p_est, 500))
 quantile(all_p, c(0.5, 0.025, 0.975))
-# higher too, I think expected because my data is enriched for most significant cases
-##       50%      2.5%     97.5% 
-## 0.5365942 0.3914681 0.6757716 
+##         50%        2.5%       97.5% 
+## 0.005180223 0.000555023 0.014349786 
 all_alpha <- sapply(multi_auto[keep], function(auto) tail(auto$path_alpha_est, 500))
 quantile(all_alpha, c(0.5, 0.025, 0.975))
-##       50%      2.5%     97.5% 
-## -1.199174 -1.367569 -1.098398
+##        50%       2.5%      97.5% 
+## -1.2123923 -1.5000000 -0.5682036 
 ## # this fails, unclear why, but resulting bsamp is list with "0-column" matrices
 ## bsamp <- lapply(multi_auto[keep], function(auto) auto$sample_beta)
 ## all_r2 <- do.call("cbind", lapply(seq_along(bsamp), function(ic) {
-##   b1 <- bsamp[[ic]]
-##   Rb1 <- apply(b1, 2, function(x)
-##     coef_shrink * bigsparser::sp_prodVec(corr, x) + (1 - coef_shrink) * x)
-##   b2 <- do.call("cbind", bsamp[-ic])
-##   b2Rb1 <- as.matrix(Matrix::crossprod(b2, Rb1))
+##     b1 <- bsamp[[ic]]
+##     Rb1 <- apply(b1, 2, function(x)
+##         coef_shrink * bigsparser::sp_prodVec(corr, x) + (1 - coef_shrink) * x)
+##     b2 <- do.call("cbind", bsamp[-ic])
+##     b2Rb1 <- as.matrix(Matrix::crossprod(b2, Rb1))
 ## }))
 ## quantile(all_r2, c(0.5, 0.025, 0.975))
+
 beta_auto <- rowMeans(sapply(multi_auto[keep], function(auto) auto$beta_est))
 pred_auto <- big_prodVec(G, beta_auto, ind.col = df_beta[["_NUM_ID_"]])
 pcor(pred_auto, y, NULL)^2
-# [1] NA NA NA
-cor(pred_auto, y)^2
-# [1] 0.004926419
+## [1] 0.017651880 0.002007581 0.047902414
+## cor(pred_auto, y)^2
 
 # skipped bits about fine-mapping, revisit when data is improved!
 
+save.image()
---
+++ prs/ldpred.q
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p ochoalab --account=ochoalab
+#SBATCH --job-name=ldpred
+#SBATCH --output=ldpred.out
+## --mem: 256G was submittable but didn't run (not enough resources), 512G didn't submit at all; 64G was not enough
+## with one thread, 64G was not enough (OOM), 200 didn't run (not enough resources)
+#SBATCH --mem=150G
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=alejandro.ochoa@duke.edu
+#SBATCH --mail-type=END,FAIL
+
+module load R/4.1.1-rhel8
+
+time Rscript ldpred.R 
+
+module unload R/4.1.1-rhel8
